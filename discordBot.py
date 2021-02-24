21097e26e3ea393f51e509e3a222e4d4e25248d5
Máté Pados <46728419+padi48@users.noreply.github.com>
2021-02-24 16:15:39 +0100

    Create discordBot.py

---
+++ discordBot.py
@@ -0,0 +1,28 @@
+'''
+very basic discord bot being developed by me
+currently I'm just learning, so it doesn't have 
+any purpose, but I'm opened for suggestions!
+'''
+
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello')
+
+    if message.content.startswith('$info'):
+        await message.channel.send('Discord BOT, coded by PADi\n(https://github.com/padi48)')
+
+
+client.run(os.getenv('TOKEN'))
