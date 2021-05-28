fd19cceb31bd2b300ec0f09b6bb4d5b5c521aac3
Máté Pados <46728419+padi48@users.noreply.github.com>
2021-05-28 12:15:55 +0200

    My first password generator

---
+++ Python OPP/passwordgen.py
@@ -0,0 +1,23 @@
+import string
+import random
+
+class User:
+    def __init__(self,name):
+        self.name = name
+        self.pw_length = int(input("Lenght of your password: "))
+
+class Password(User):
+    def __init__(self, name):
+        super().__init__(name)
+        self.password = ''
+
+        while len(self.password) < self.pw_length:
+            random_char = random.choice(string.printable)
+            self.password = self.password + random_char
+
+
+    def get_pw_of(self):
+        print('Password of {} is: {}'.format(self.name, self.password))
+
+if __name__ == '__main__':
+    Password('Matthew').get_pw_of()
