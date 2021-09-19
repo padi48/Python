7b59d57f00ae806ff31cb83c172b63b7479e98cb
Máté Pados <46728419+padi48@users.noreply.github.com>
2021-09-19 14:37:37 +0200

    Create user.py

---
+++ Python OPP/Banking system/user.py
@@ -0,0 +1,48 @@
+import random
+import getpass
+
+class User(object):
+    def __init__(self):
+        print()
+
+    def register(self):
+        print("***REGISTER***")
+        file = open("accounts.txt", "a+")
+
+        self.firstname = input("Enter your first name: ")
+        self.lastname = input("Enter your last name: ")
+        self.password = getpass.getpass(prompt="Enter password: ", stream="*")
+
+        self.id = random.randint(1000,9999)
+
+        for line in file:
+            line.split()
+            if self.id in file:
+                self.id = random.randint(1000,9999)
+
+
+        print()
+        print("Successful registration!")
+        print("Your details:")
+        print("First name:", self.firstname)
+        print("Last name:", self.lastname)
+        print("Password: ")
+        print("Account ID:", self.id)
+
+        text = f"{self.firstname} {self.lastname} | {self.password} | {self.id}\n"
+        file.write(text)
+        file.close()
+
+
+    def login(self):
+        print("Enter your ID: ")
+        id = input()
+
+        with open("accounts.txt", "r") as file:
+            for line in file:
+                line.split()
+                if id in line:
+                    print("Logged in as:")
+                    print(line)
+
+        file.close()
