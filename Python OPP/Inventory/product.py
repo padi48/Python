d156606b005fe1cface8972980a2b9daaea4df5d
Máté Pados <46728419+padi48@users.noreply.github.com>
2021-11-09 11:14:32 +0100

    Create product.py

---
+++ Python OPP/Inventory/product.py
@@ -0,0 +1,11 @@
+from main import Product
+
+#name, id, price, quantity
+
+apple = Product("Apple", 112, 0.99, 245)
+banana = Product("Banana", 143, 1.19, 188)
+cherry = Product("Cherry", 115, 0.59, 768)
+milk = Product("Milk", 426, 5.99, 837)
+coffee = Product("Coffee", 981, 3.89, 230)
+keyboard = Product("Keyboard", 776, 159.99, 21)
+shirts = Product("Shirt", 451, 11.49, 999)
